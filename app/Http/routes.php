9716fee946810b5bc61be3e18af5c55b38d6532b
Stefan Trajkovic <trj.stefan@gmail.com>
2016-06-27 17:25:31 +0200

    Edited .httacess file.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,13 +11,16 @@
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-
+Route::get('test', 'UsersController@test');
 
 Route::group(['prefix' => 'api/v1'], function () {
+
+    //Route::get('test', 'UsersController@test');
 
     Route::resource('cities', 'CityController');
     Route::resource('stations', 'StationController');
